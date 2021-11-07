55c350bef7791bb956f42738bf7598a1438a16b3
arnabtarwani <arnab.tarwani@gmail.com>
2021-11-07 03:20:40 +0000

    changed anchor width

--- src/components/Home/SocialLinks/SocialLinks.tsx
+++ src/components/Home/SocialLinks/SocialLinks.tsx
@@ -6,12 +6,12 @@ export const SocialLinks = () => {
     return (
         <>
             <div className="mt-2 flex justify-start items-center">
-                <a href="https://github.com/arnabtarwani" target="_blank"><FaGithub className="mr-4" size={20} /></a>
-                <a href="https://linkedin.com/in/arnabtarwani" target="_blank"><FaLinkedin className="mr-4" size={20} /></a>
-                <a href="https://twitter.com/arnabtarwani" target="_blank"><FaTwitter className="mr-4" size={20} /></a>
-                <a href="https://medium.com/@arnab.tarwani" target="_blank"><FaMedium className="mr-4" size={20} /></a>
-                <a href="https://www.instagram.com/arnabtarwani/" target="_blank"><FaInstagram className="mr-4" size={20} /></a>
-                <a href="mailto:arnab@tarwani.net" target="_blank"><HiOutlineMail className="mr-4" size={22} /></a>
+                <a href="https://github.com/arnabtarwani" target="_blank" className="mr-4"><FaGithub size={20} /></a>
+                <a href="https://linkedin.com/in/arnabtarwani" target="_blank" className="mr-4"><FaLinkedin size={20} /></a>
+                <a href="https://twitter.com/arnabtarwani" target="_blank" className="mr-4"><FaTwitter size={20} /></a>
+                <a href="https://medium.com/@arnab.tarwani" target="_blank" className="mr-4"><FaMedium size={20} /></a>
+                <a href="https://www.instagram.com/arnabtarwani/" target="_blank" className="mr-4"><FaInstagram className="mr-4" size={20} /></a>
+                <a href="mailto:arnab@tarwani.net" target="_blank" className="mr-4"><HiOutlineMail size={22} /></a>
             </div>
         </>
     )
